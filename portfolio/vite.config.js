954f3da792b9697ec3e70543f29b13352214ee55
Smil <149633447+IsaiasSorriso@users.noreply.github.com>
2024-12-12 15:14:29 -0300

    se pa é isso

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/IsaiasSorriso.github.io"
+  base: "/IsaiasSorriso.github.io/portfolio"
 })
